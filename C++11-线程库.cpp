de9bf7c34b2261d2998b41b1cd75cbe973fb0e6a
webary <813577991@qq.com>
2016-03-13 14:25:14 +0800

    C++11线程库的简单用法示例

---
+++ C++11-线程库.cpp
@@ -0,0 +1,69 @@
+///示例1
+#include <iostream>
+#include <thread>
+#include <string>
+using namespace std;
+
+void hello(const string& str)
+{
+    cout << str;
+}
+
+int main2()
+{
+    thread t(hello, "hello ");
+    thread t2(hello, "world!");
+    t.join();
+    t2.join();
+    return 0;
+}
+///示例2
+#include <windows.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+void mbox(string msg, string caption = "温馨提示")
+{
+    MessageBox(0, msg.c_str(), caption.c_str(), 0);
+}
+
+void f1(int n, mutex &mtx_cout, volatile bool& stop)
+{
+    for (int i = 0; i < 5; ++i) {
+        mtx_cout.lock();
+        cout << "Thread " << n << " executing" << endl;
+        mtx_cout.unlock();
+        this_thread::sleep_for(chrono::milliseconds(10));
+    }
+    thread *t = new thread(mbox, "线程1执行中，等待结束...", "caption");
+    while(!stop)
+        this_thread::sleep_for(chrono::milliseconds(10));
+}
+
+void f2(int& n, mutex& mtx_cout)
+{
+    for (int i = 0; i < 5; ++i) {
+        mtx_cout.lock();
+        cout << "Thread 2 executing" << endl;
+        mtx_cout.unlock();
+        ++n;
+        this_thread::sleep_for(chrono::milliseconds(5));
+    }
+}
+
+int main()
+{
+    mutex mtx_cout; //创建输出的互斥量，保证多个线程正常向控制台输出
+    volatile bool stop = 0;
+    int n = 0;
+    thread t1(f1, n + 1, ref(mtx_cout), ref(stop)); // n+1 pass by value
+    thread t2(f2, ref(n), ref(mtx_cout)); // n pass by reference
+    mbox("按确认退出线程1");
+    stop = 1;
+    t1.join();
+    t2.join();
+    cout << "Final value of n is " << n << endl;
+    return 0;
+}
