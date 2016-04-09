c37cd18b7f8cbf7863d74be0c44678b456a03dfc
webary <813577991@qq.com>
2016-04-09 18:32:02 +0800

    update code format and count code length of each folder

--- 统计文件行数(代码量).cpp
+++ 统计文件行数(代码量).cpp
@@ -1,8 +1,10 @@
-#include <ctime> //clock()
-#include <string>
-#include <vector>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <ctime>     //clock()
 #include <windows.h> //system(),DeleteFile()
 using namespace std;
 
@@ -14,11 +16,11 @@ vector<string> getDirFileList(const string& dir)
     vector<string> res;
     string fileName;
     ifstream fin("filelist");
-    while(getline(fin, fileName)) {
+    while (getline(fin, fileName)) { //get one line (a file name with path)
         string::size_type pos = fileName.rfind('.'); //find the last char '.'
-        if(pos != string::npos) {
-            string fmt = fileName.substr(pos); //get file format
-            if(fmt == ".cpp" || fmt == ".h" || fmt == ".hpp" || fmt == ".c")
+        if (pos != string::npos) {
+            string fmt = fileName.substr(pos); //get the format of the file
+            if (fmt == ".cpp" || fmt == ".h" || fmt == ".hpp" || fmt == ".c")
                 res.push_back(fileName);
         }
     }
@@ -31,16 +33,16 @@ vector<string> getDirFileList(const string& dir)
 int getFileLine(string fName, pair<int, int>& lines)
 {
     string::size_type pos = fName.rfind('.'); //find the last char '.'
-    if(pos == string::npos) //ignore files with no file format
+    if (pos == string::npos) //ignore files with no file format
         return 0;
     string fmt = fName.substr(pos); //get file format
-    if(fmt != ".cpp" && fmt != ".h" && fmt != ".hpp" && fmt != ".c")
+    if (fmt != ".cpp" && fmt != ".h" && fmt != ".hpp" && fmt != ".c")
         return 0;
     ifstream fin(fName.c_str()); //open the file to read the content
-    int cnt = 0, cnt_space = 0; //file lines, space lines
+    int cnt = 0, cnt_space = 0;  //file lines, space lines
     string str;
-    while(getline(fin, str)) { //read a line every time, stastic the lines
-        if(str.find_first_not_of(" \t") == str.npos) //check if is a space line
+    while (getline(fin, str)) {  //read a line every time, stastic the lines
+        if (str.find_first_not_of(" \t") == str.npos) //check if is a space line
             ++cnt_space;
         ++cnt;
     }
@@ -53,7 +55,7 @@ int getFileLine(string fName, pair<int, int>& lines)
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2) { //have no command parameters with file names or paths
+    if (argc < 2) { //have no command parameters with file names or paths
         cout << "Oops, please open me in another way! such as drag files or"
              " paths to my '.exe' file..." << endl;
         cin.get();
@@ -61,25 +63,33 @@ int main(int argc, char *argv[])
     }
     int sum = 0, sum_space = 0; //total lines, total space lines
     int fileNums = 0; //the number of file
-    for(int i = 1; i < argc; ++i) {
+    unordered_map<string, int> folderLines;
+    for (int i = 1; i < argc; ++i) {
         string fName = argv[i];
         pair<int, int> lines;
         vector<string> fileList;
-        if(fName.rfind('.') == string::npos) { //recursively read a directoy
+        string::size_type pos = fName.rfind('.'); //find the last char '.'
+        if (pos != string::npos) { //if there is a '.' in the path name
+            string fmt = fName.substr(pos); //get file format or ...
+            if (fmt == ".cpp" || fmt == ".h" || fmt == ".hpp" || fmt == ".c")
+                fileList.push_back(fName);
+            else
+                fileList = getDirFileList(fName);
+        } else
             fileList = getDirFileList(fName);
-        } else {
-            fileList.push_back(fName);
-        }
-        for(auto &elem : fileList) { //deal each file from the file list
-            if(getFileLine(elem, lines)) {
+        for (auto &elem : fileList) { //deal with each file from the file list
+            if (getFileLine(elem, lines)) {
                 ++fileNums;
                 sum += lines.first;
                 sum_space += lines.second;
+                folderLines[elem.substr(0, elem.rfind('\\'))] += lines.first;
             }
         }
     }
     cout << "\n" << fileNums << " files total: " << sum << " lines!\n"
          << sum_space << " space lines, left " << sum - sum_space << " lines\n";
+    for (auto &folder : folderLines) //output code lines of each folder
+        cout << setw(6) << folder.second << ": [" << folder.first << "]" << endl;
     cout << "\ntime cost: " << clock() / 1000.0 << " s" << endl;
     cin.get();
     return 0;
